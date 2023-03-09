b61063fe2de9f67fb4dbfb8f2b485a7fa3ecb37d
Jay Kang <jaykang0395@gmail.com>
2023-03-09 16:50:24 +1300

    Refactored the useEffect to implement api agent instead and implemented convertUtcToLocal utils

--- client/src/components/jobs/jobList/JobListPage.component.tsx
+++ client/src/components/jobs/jobList/JobListPage.component.tsx
@@ -1,6 +1,7 @@
-import axios from "axios";
 import React, { useEffect, useState } from "react";
+import agent from "../../../app/api/agent";
 import { Job } from "../../../app/models/job";
+import { convertUtcToLocal } from "../../../app/utils/convertUtcToLocal.util";
 import JobList from "./JobList.component";
 import "./jobListPage.styles.css";
 
@@ -8,10 +9,19 @@ const JobListPage = () => {
   const [jobs, setJobs] = useState<Job[]>();
 
   useEffect(() => {
-    axios.get<Job[]>("http://localhost:5000/api/jobs").then((response) => {
-      setJobs(response.data);
-    });
+    const loadJobs = async () => {
+      const response = await agent.Jobs.list();
+      const updatedJobs = response.map((job) => {
+        const localDate = convertUtcToLocal(job.date);
+        return { ...job, date: localDate };
+      });
+
+      setJobs(updatedJobs);
+    };
+
+    loadJobs();
   }, [setJobs]);
+
   return (
     <div className="job-list-page">
       {jobs ? <JobList jobs={jobs} /> : <p>Loading...</p>}
