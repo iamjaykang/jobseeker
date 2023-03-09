5c0b3324f6abfdfaacc54792c4df78dc12554b34
Jay Kang <jaykang0395@gmail.com>
2023-03-10 00:07:13 +1300

    Added hiddenPath

--- client/src/app/layout/App.layout.tsx
+++ client/src/app/layout/App.layout.tsx
@@ -2,20 +2,21 @@ import React from "react";
 import Header from "./Header.layout";
 import Footer from "./Footer.layout";
 import "./styles.css";
-import { useParams } from "react-router-dom/";
+import { ScrollRestoration, useParams } from "react-router-dom/";
 import JobSearchForm from "./JobSearchForm.layout";
 import { Outlet, useLocation } from "react-router";
 import HomePage from "../../components/home/HomePage.component";
 
 function App() {
   const location = useLocation();
-  const {id} = useParams();
-  const hiddenPaths = [`/browse-jobs/${id}`];
+  const { id } = useParams();
+  const hiddenPaths = [`/browse-jobs/${id}`, "/post-job"];
   const isSearchFormVisible = !hiddenPaths.some((path) =>
     location.pathname.startsWith(path)
   );
   return (
     <div className="app">
+      <ScrollRestoration />
       <Header />
       {isSearchFormVisible && (
         <div className="app__search">
