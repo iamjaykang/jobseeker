36f2a21d4867e9e7e49e61c5744e2affe868a486
Jay Kang <jaykang0395@gmail.com>
2023-03-09 15:42:18 +1300

    Refactor form in homepage to a separate component

--- client/src/components/home/HomePage.component.tsx
+++ client/src/components/home/HomePage.component.tsx
@@ -4,30 +4,10 @@ import "./homePage.styles.css";
 const HomePage = () => {
   return (
     <div className="homepage">
-      <h1 className="homepage__heading">Find Your Dream Job</h1>
+      <h1 className="homepage__heading">Home Page</h1>
       <p className="homepage__subheading">
-        Search through our job listings and find the right fit for you.
+        Home Content
       </p>
-      <form className="homepage__form">
-        <input
-          className="homepage__form-input"
-          type="text"
-          placeholder="Enter keywords"
-        />
-        <input
-          className="homepage__form-input"
-          type="text"
-          placeholder="Any classification"
-        />
-        <input
-          className="homepage__form-input"
-          type="text"
-          placeholder="Enter city"
-        />
-        <button className="homepage__form-button" type="submit">
-          Search
-        </button>
-      </form>
     </div>
   );
 };
