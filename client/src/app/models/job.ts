e661076fd69b7aa10d2ad23088fd508436f59e59
Jay Kang <jaykang0395@gmail.com>
2023-03-09 15:39:55 +1300

    Add interface for Job object in models folder

---
+++ client/src/app/models/job.ts
@@ -0,0 +1,11 @@
+export interface Job {
+  id: string;
+  title: string;
+  date: string;
+  description: string;
+  jobType: string;
+  postedBy: string;
+  salary: string;
+  experienceLevel: string;
+  city: string;
+}
