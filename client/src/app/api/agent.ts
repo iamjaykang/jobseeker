e3e5471f65b0ab5f16698de439a783f7416a128f
Jay Kang <jaykang0395@gmail.com>
2023-03-11 00:04:51 +1300

    Added serror routes for testing and implemented error handling using interceptors in API agent

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -1,5 +1,9 @@
-import axios, { AxiosResponse } from "axios";
+import axios, { AxiosError, AxiosResponse } from "axios";
+import { toast } from "react-toastify";
 import { Job, JobFormValues } from "../models/job.model";
+import { router } from "../router/Routes";
+import { setServerError } from "../stores/common/common.action";
+import { store } from "../stores/store";
 
 const sleep = (delay: number) => {
   return new Promise((resolve) => {
@@ -9,10 +13,50 @@ const sleep = (delay: number) => {
 
 axios.defaults.baseURL = "http://localhost:5000/api";
 
-axios.interceptors.response.use(async (res) => {
-  if (process.env.NODE_ENV === "development") await sleep(1000);
-  return res;
-});
+axios.interceptors.response.use(
+  async (res) => {
+    if (process.env.NODE_ENV === "development") await sleep(1000);
+    return res;
+  },
+  (error: AxiosError) => {
+    const { data, status, config } = error.response as AxiosResponse;
+
+    switch (status) {
+      case 400:
+        if (config.method === "get" && data.errors.hasOwnProperty("id")) {
+          router.navigate("/not-found");
+        }
+        if (data.errors) {
+          const modalStateErrors = [];
+          for (const key in data.errors) {
+            if (data.errors[key]) {
+              modalStateErrors.push(data.errors[key]);
+            }
+          }
+
+          throw modalStateErrors.flat();
+        } else {
+          toast.error(data);
+        }
+        break;
+      case 401:
+        toast.error("unauthorised");
+        break;
+      case 403:
+        toast.error("forbidden");
+        break;
+      case 404:
+        router.navigate("/not-found");
+        break;
+      case 500:
+        store.dispatch(setServerError(data));
+        router.navigate("/server-error");
+        break;
+    }
+
+    return Promise.reject(error);
+  }
+);
 
 const responseBody = <T>(response: AxiosResponse<T>) => response.data;
 
--- client/src/app/router/Routes.tsx
+++ client/src/app/router/Routes.tsx
@@ -1,5 +1,8 @@
 
-import { createBrowserRouter, RouteObject } from "react-router-dom";
+import { createBrowserRouter, Navigate, RouteObject } from "react-router-dom/";
+import NotFound from "../../components/errors/notFound/NotFound.component";
+import ServerError from "../../components/errors/serverError/ServerError.component";
+import TestErrors from "../../components/errors/TestError.component";
 import JobDetails from "../../components/jobs/details/JobDetails.component";
 import JobForm from "../../components/jobs/form/JobForm.component";
 import JobListPage from "../../components/jobs/jobList/JobListPage.component";
@@ -14,6 +17,10 @@ export const routes: RouteObject[] = [
       { path: "browse-jobs/:id", element: <JobDetails /> },
       { path: "post-job", element: <JobForm key="create" /> },
       { path: "manage/:id", element: <JobForm key="manage" /> },
+      { path: "errors", element: <TestErrors /> },
+      { path: "not-found", element: <NotFound /> },
+      { path: "server-error", element: <ServerError /> },
+      { path: "*", element: <Navigate replace to='/not-found' /> },
     ],
   },
 ];
