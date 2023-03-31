fd041e630b7ec5ec8ab92dcfc90cb526ff810494
Jay Kang <jaykang0395@gmail.com>
2023-03-31 21:44:04 +1300

    Updated Link to follow updated route

--- client/src/components/jobPosts/details/JobPostDetailsActions.component.tsx
+++ client/src/components/jobPosts/details/JobPostDetailsActions.component.tsx
@@ -1,16 +1,19 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom/";
 import { JobPost } from "../../../app/models/jobPost.model";
+import { selectUser } from "../../../app/stores/users/user.selector";
 
 interface Props {
   jobPost: JobPost;
 }
 
 const JobPostDetailsActions = ({ jobPost }: Props) => {
+  const currentUser = useSelector(selectUser);
   return (
     <div className="job-details-actions">
       <div className="job-details-actions__buttons">
-        <Link to={`/job/${jobPost.id}/apply`} className="job-details-actions__apply">Apply</Link>
+        <Link to={`/job/${jobPost.id}/apply/${currentUser.username}`} className="job-details-actions__apply">Apply</Link>
         <button className="job-details-actions__save">Save Job</button>
       </div>
       <p className="job-details-actions__applicants">
